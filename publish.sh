87153d64cd38a0c2f401f36f13d3689bf50370ac
Kyle Mahan <kyle.mahan@gmail.com>
2013-11-21 21:16:21 -0800

    Update

--- publish.sh
+++ publish.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+git add .
+git commit -m "Update"
+git push origin master
 jekyll build --config _config.yml,_deploy.yml
 rsync -av --delete --exclude piwik _site/ orin.kylewm.com:/srv/www/kylewm.com/public_html
